5166158e86ad324315723dbd723c951c39086607
Rafael <>
2018-11-12 21:57:08 -0300

    Tabelas dinamicas

--- app/src/main/java/com/example/ogn/conexaovida/doacoesActivity.java
+++ app/src/main/java/com/example/ogn/conexaovida/doacoesActivity.java
@@ -7,11 +7,17 @@ import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.concurrent.ExecutionException;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 
 public class doacoesActivity extends menuActivity {
@@ -22,40 +28,67 @@ public class doacoesActivity extends menuActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doacoes);
-//
-//        tabelaDoacoes t1 = (tabelaDoacoes) getSupportFragmentManager().findFragmentById(R.id.fragment);
-//        t1.setDados("Jair Bolsonaro","O+","1");
-////
-//        tabelaDoacoes t2 = (tabelaDoacoes) getSupportFragmentManager().findFragmentById(R.id.fragment2);
-//        t2.setDados("Alexei Nikolaevich","AB-","4");
 
-
-//        try {
-//             lista  = pedido.getPedidos(context);
-//        } catch (ExecutionException | JSONException | InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//        Log.d("lmaoo",lista);
-
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-
-//Replace the contents of the container with the new fragmentTest
-        for (int i = 0; i < 2; i++){
-            ft.add(R.id.teste, new tabelaDoacoes());
-        }
-
-//Complete the changes
-        ft.commit();
-
-
-    }
-
-    public void detalhes(View V) throws ExecutionException, InterruptedException {
+        String lista = null;
 
         Context context = this;
 
-        pedido.Show(context, V.getTag().toString());
+        JSONArray jsonArray;
 
+        TextView twNome;
+        TextView twSangue;
+        TableRow tr;
+        Button detalhes;
+
+        TableLayout tl = findViewById(R.id.tableLayout);
+
+        try {
+            lista  = pedido.getPedidos(context);
+
+            Log.d("fuck",lista);
+
+            JSONObject object = new JSONObject(lista);
+            jsonArray  = object.getJSONArray("data");
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject pedido = jsonArray.getJSONObject(i);
+
+                twNome = new TextView(this);
+                twNome.setText(pedido.get("paciente").toString());
+                twNome.setTextSize(18);
+                twNome.setTextColor(0);
+
+                twSangue = new TextView(this);
+                twSangue.setText(pedido.get("tipo_sanguineo_id").toString());
+                twSangue.setTextSize(18);
+
+                detalhes = new Button(this);
+                detalhes.setText("Detalhes");
+                detalhes.setTag(pedido.get("id").toString());
+
+
+                tr = new TableRow(this);
+
+                tr.addView(twNome);
+                tr.addView(twSangue);
+                tr.addView(detalhes);
+                tl.addView(tr);
+//                Log.d("Nome:", JSONLivro.get("nome").toString());
+//                Log.d("Ano:", JSONLivro.get("ano").toString());
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void detalhar(View V) throws ExecutionException, InterruptedException {
+        Context context = this;
+
+        pedido.Show(context, V.getTag().toString());
     }
 }
--- app/src/main/java/com/example/ogn/conexaovida/validar.java
+++ app/src/main/java/com/example/ogn/conexaovida/validar.java
@@ -1,19 +1,53 @@
 package com.example.ogn.conexaovida;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
 public class validar {
 
-    public static boolean cadastroUser(Context context, String nome, String email, String telefone,
+    public static boolean cadastroUser(Context c, String nome, String email, String telefone,
                                        String cidade, String password, String confirmarPassword)
     {
-       if (nome.equals("") || email.equals("") || telefone.equals("") ||
+        //Checa se é nulo
+        if (nome.equals("") || email.equals("") || telefone.equals("") ||
                 cidade.equals("") || password.equals("") || confirmarPassword.equals("")){
-           Toast.makeText(context, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+           Toast.makeText(c, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
 
            return false;
-       } else
+        //Checa se as senhas batem
+        } else if (!password.equals(confirmarPassword)){
+           Toast.makeText(c, "As senhas informadas não são iguais", Toast.LENGTH_SHORT).show();
+
+           return false;
+        //Checa o tamanho dos campos
+       } else if (!validar.size(c, "nome",nome,1,100) || !validar.size(c,"email",email,1,100) ||
+                !validar.size(c,"telefone", telefone,10,11) || !validar.size(c,"cidade",cidade,1,100) ||
+                !validar.size(c,"senha",password,6,20)
+                ) {
+
+        return false;
+    }
+
+       else
            return  true;
     }
+
+    public static boolean size(Context c, String nome, String campo, int min, int max){
+
+        if (campo.length() > max){
+            Toast.makeText(c, "O campo " + nome + " deve conter menos de " +max+ " caracteres", Toast.LENGTH_SHORT).show();
+
+            return false;
+        } else if(campo.length() < min){
+            Toast.makeText(c, "O campo " + nome + " deve conter mais de " +min+ " caracteres", Toast.LENGTH_SHORT).show();
+
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+
 }
